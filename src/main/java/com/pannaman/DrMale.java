a89c5bfa9aa7a0b06a9289aba379729184e36cea
Bruce Pannaman <bpannaman@PEG-MAC-007809.local>
2015-01-15 11:39:12 +0000

    More Testing and Better Testing

--- src/main/java/com/pannaman/DrMale.java
+++ src/main/java/com/pannaman/DrMale.java
@@ -3,7 +3,16 @@ package com.pannaman;
 
 import org.apache.pig.EvalFunc;
 import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
 import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.pig.data.DataType;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+import org.apache.pig.impl.logicalLayer.FrontendException;
+import org.apache.pig.FuncSpec;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 
 @Description(
@@ -12,26 +21,71 @@ import org.apache.hadoop.hive.ql.exec.Description;
 )
 public class DrMale extends EvalFunc<String> {
 
-    public String exec(Tuple input) {
+    public String exec(Tuple input) throws IOException {
 
-        String Doc = input.toString().toLowerCase();
 
-        if (Doc == "dr-male") {
-            return new String("Male");
-        } else if (Doc == "dr-female") {
-            return new String("Female");
-        } else if (Doc == "mr") {
-            return new String("Male");
-        } else if (Doc == "miss") {
-            return new String("Female");
-        } else if (Doc == "mrs") {
-            return new String("Female");
-        } else if (Doc == "ms") {
-            return new String("Female");
-        } else {
+        if (input == null || input.size() == 0 || input.get(0) == null || input.get(0) == "") {
             return null;
         }
 
 
+        try {
+
+            String Doc = (String) input.get(0);
+            String female = "Female";
+            String male = "Male";
+
+            if (Doc.toLowerCase().equals("dr-male") ) {
+                return male;
+            }
+
+            if (Doc.toLowerCase().equals("dr-female")) {
+                return female;
+            }
+
+            if (Doc.toLowerCase().equals( "mr")) {
+                return male;
+            }
+
+            if (Doc.toLowerCase().equals("miss")) {
+                return female;
+            }
+
+            if (Doc.toLowerCase().equals("mrs")) {
+                return female;
+            }
+
+            if (Doc.toLowerCase().equals("ms")) {
+                return female;
+            }
+            else {
+                return null;
+            }
+        } catch (Exception e) {
+            throw new IOException("DrMale has an error " + e);
+        }
+
+    }
+
+    /**
+     * This method gives a name to the column.
+     *
+     * @param input - schema of the input data
+     * @return schema of the input data
+     */
+    @Override
+    public Schema outputSchema(Schema input) {
+        return new Schema(new Schema.FieldSchema(getSchemaName(this.getClass().getName().toLowerCase(), input), DataType.CHARARRAY));
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.pig.EvalFunc#getArgToFuncMapping()
+     */
+    @Override
+    public List<FuncSpec> getArgToFuncMapping() throws FrontendException {
+        List<FuncSpec> funcList = new ArrayList<FuncSpec>();
+        funcList.add(new FuncSpec(this.getClass().getName(), new Schema(new Schema.FieldSchema(null, DataType.CHARARRAY))));
+
+        return funcList;
     }
 }
--- src/test/java/com/pannaman/DrMaleTest.java
+++ src/test/java/com/pannaman/DrMaleTest.java
@@ -1,34 +1,103 @@
 package com.pannaman;
 
 
+import junit.framework.TestCase;
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.pig.data.DefaultTupleFactory;
 import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
 import org.junit.Test;
 import junit.framework.Assert;
 import org.apache.hadoop.io.Text;
+import com.pannaman.DrMale;
 
-public class DrMaleTest
-{
-    /*
+import java.io.IOException;
+
+import org.apache.pig.data.BinSedesTuple;
+import org.joda.time.ReadableInstant;
+
+public class DrMaleTest extends TestCase {
+
+    private TupleFactory tupleFact = TupleFactory.getInstance();
+
+
     @Test
-    public void testUDF() {
-        DrMale example = new DrMale();
-        Assert.assertEquals("Male", example.exec(new Tuple("Dr-Male") {
-        }).toString());
+    public void testUDF() throws Exception {
+        try { // NEW INSTANCE OF THE METHOD
+            DrMale example = new DrMale();
+
+            //SETTING WHAT YOU WANT IN AND OUT OF TEST
+            String in = "miss";
+            String expected = "Female";
+
+            //MAKING THE INPUT STRING INTO A TUPLE THAT IS NEEDED FOR UDF INPUT
+            Tuple input = DefaultTupleFactory.getInstance().newTuple(in);
+
+            //RUN THIS INPUT THROUGH THE FUNCTION
+            String output = example.exec(input);
+
+            //IF INPUT AND OUTPUT MATCH WHAT YOU ARE EXPECTING, PASS THE TEST
+            assertTrue(output.equals(expected));
+
+        } catch (Exception e) {
+            System.out.println("Dr-Male test has thrown and exception on the testUDF method " + e);
+        }
+
     }
 
     @Test
-    public void testUDFNullCheck() {
-        DrMale example = new DrMale();
-        Assert.assertNull(example.exec(null));
+    public void testUDFNullCheck() throws IOException {
+        try {//CREATE NEW INSTANCE OF METHOD
+            DrMale example = new DrMale();
+
+            //PASS TEST IF NULL IS RECEIVED
+            assertNull(example.exec(null));
+        } catch (Exception e) {
+            System.out.println("Dr-Male test has thrown and exception on the testUDFNullCheck method " + e);
+        }
     }
 
-    public void testUDFEmptyStringCheck() {
-       DrMale example = new DrMale();
-        Assert.assertNull(example.exec(new Tuple("") {
-        }));
+    public void testUDFEmptyStringCheck() throws Exception {
+        try {// NEW INSTANCE OF THE METHOD
+            DrMale example = new DrMale();
+
+            //SETTING WHAT YOU WANT IN AND OUT OF TEST
+            String in = "";
+            String expected = null;
+
+            //MAKING THE INPUT STRING INTO A TUPLE THAT IS NEEDED FOR UDF INPUT
+            Tuple input = DefaultTupleFactory.getInstance().newTuple(in);
+
+            //RUN THIS INPUT THROUGH THE FUNCTION
+            String output = example.exec(input);
+
+            //IF INPUT AND OUTPUT MATCH WHAT YOU ARE EXPECTING, PASS THE TEST
+            assertNull(output);
+        } catch (Exception e) {
+            System.out.println("Dr-Male test has thrown and exception on the testUDFEmptyStringCheck method " + e);
+        }
     }
 
-*/
+    public void testUDFSillyStringCheck() throws Exception {
+        try {// NEW INSTANCE OF THE METHOD
+            DrMale example = new DrMale();
 
+            //SETTING WHAT YOU WANT IN AND OUT OF TEST
+            String in = "ievbifubcibew";
+            String expected = "Java Sucks Balls";
+
+            //MAKING THE INPUT STRING INTO A TUPLE THAT IS NEEDED FOR UDF INPUT
+            Tuple input = DefaultTupleFactory.getInstance().newTuple(in);
+
+            //RUN THIS INPUT THROUGH THE FUNCTION
+            String output = example.exec(input);
+
+            //IF INPUT AND OUTPUT MATCH WHAT YOU ARE EXPECTING, PASS THE TEST
+            assertTrue(output.equals(expected));
+
+        } catch (Exception e) {
+            System.out.println("Dr-Male test has thrown and exception on the testUDFEmptyStringCheck method " + e);
+        }
+    }
 
 }
